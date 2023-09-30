3f5147809d2d9b97fd306d7f3a421a620b27d6b4
mxdnxght <cloudyfg@gmail.com>
2023-09-30 22:25:06 +0300

    Added inheritance from Entity.cs. Moved enum UserType from class scope to namespace scope.

--- src/ScienceAtrium/ScienceAtrium.Domain/UserAggregate/User.cs
+++ src/ScienceAtrium/ScienceAtrium.Domain/UserAggregate/User.cs
@@ -1,24 +1,19 @@
-﻿using System;
-namespace ScienceAtrium.Domain.UserAggregate
+﻿using ScienceAtrium.Domain.Entities;
+namespace ScienceAtrium.Domain.UserAggregate;
+
+public class User : Entity
 {
-	public class User
-	{
-		public User(Guid id) : base(id)
-        {
-		}
-
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
-        public UserType UserType { get; set; }
-        public string PhoneNumber { get; set; }
-        
-        public enum UserType
-        {
-            Customer,
-            Executor
-        }
+    public User(Guid id) : base(id)
+    {
     }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public UserType UserType { get; set; }
 }
-
+public enum UserType
+{
+    Customer,
+    Executor
+}
