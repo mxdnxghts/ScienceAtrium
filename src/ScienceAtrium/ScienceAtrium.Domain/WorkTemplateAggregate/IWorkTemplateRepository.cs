0fecbf35cd1252a7d2b52466db6106acac54bd58
Vladimir Dmitriev <vldmtrv05@gmail.com>
2023-09-30 18:27:18 +0300

    added entity WorkTemplate(without Order, Subject)

--- src/ScienceAtrium/ScienceAtrium.Domain/WorkTemplateAggregate/IWorkTemplateRepository.cs
+++ src/ScienceAtrium/ScienceAtrium.Domain/WorkTemplateAggregate/IWorkTemplateRepository.cs
@@ -1,5 +1,18 @@
 ﻿namespace ScienceAtrium.Domain.WorkTemplateAggregate;
 public interface IWorkTemplateRepository
 {
+    public class WorkTemplate
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public WorkType Worktype { get; set; }
+    }
+    public enum WorkType
+    {
+        CourseWork,
+        LaboratoryWork,
+        CustomWork
+    }
 
 }
