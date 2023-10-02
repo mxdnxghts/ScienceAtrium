03ff40fdb5ad2d09a48ed364e43d93dd44c23792
mxdnxght <cloudyfg@gmail.com>
2023-10-02 16:04:03 +0300

    Added fields Customer and Executor to model Order. Removed fields Surname from model User.

--- src/ScienceAtrium/ScienceAtrium.Domain/OrderAggregate/Order.cs
+++ src/ScienceAtrium/ScienceAtrium.Domain/OrderAggregate/Order.cs
@@ -1,14 +1,17 @@
 ﻿using ScienceAtrium.Domain.Entities;
+using ScienceAtrium.Domain.UserAggregate;
 
 public class Order : Entity
 {
     public Order(Guid id) : base(id)
     {
     }
-    public DateTime OrderDate { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
     public decimal TotalPrice { get; set; }
     public Paymentmethod PaymentMethod { get; set; }
     public Status Status { get; set; }
+    public User Customer { get; set; }
+    public User Executor { get; set; }
     public List<WorkTemplate> WorkTemplates { get; set; } = new();
 }
 public enum Paymentmethod
--- src/ScienceAtrium/ScienceAtrium.Domain/UserAggregate/User.cs
+++ src/ScienceAtrium/ScienceAtrium.Domain/UserAggregate/User.cs
@@ -7,7 +7,6 @@ public class User : Entity
     {
     }
     public string Name { get; set; }
-    public string Surname { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public UserType UserType { get; set; }
