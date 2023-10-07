b35d97927004c4603b31479a6fc259c5a6506cba
ttwinkleee <dudinaa964@gmail.com>
2023-10-07 18:21:04 +0300

    Added IUserRepository by Feruqerro

---
+++ src/ScienceAtrium/ScienceAtrium.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScienceAtrium.Application.Common.Interfaces;
+using ScienceAtrium.Domain.UserAggregate;
+using ScienceAtrium.Infrastructure.Data;
+using Serilog;
+using System.Linq.Expressions;
+
+namespace ScienceAtrium.Infrastructure.Repositories;
+
+public sealed class UserRepository : IUserRepository<User>
+{
+    private readonly ApplicationContext _context;
+    private readonly ILogger _logger;
+
+    public UserRepository(ApplicationContext context, ILogger logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+public IQueryable<User> All => _context.Users;
+
+    public int Create(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> CreateAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Delete(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> DeleteAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exist(System.Linq.Expressions.Expression<Func<User, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistAsync(System.Linq.Expressions.Expression<Func<User, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool FitsConditions(User? entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> FitsConditionsAsync(User? entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User Get(System.Linq.Expressions.Expression<Func<User, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetAsync(System.Linq.Expressions.Expression<Func<User, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Update(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> UpdateAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+}
