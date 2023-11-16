d15cae08412a33a4f983a180e16b8336115bb87e
mxdnxght <cloudyfg@gmail.com>
2023-11-16 22:49:40 +0300

    Registred MediatR in Application layer.

---
+++ src/ScienceAtrium/ScienceAtrium.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ScienceAtrium.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMediatR(c =>
+        {
+            c.RegisterServicesFromAssembly(typeof(MediatREntryPoint).Assembly);
+        });
+        return serviceCollection;
+    }
+}
