94dae934c5df675fdb6aa07d759e6703c9c9fb93
mxdnxght <cloudyfg@gmail.com>
2024-01-08 23:31:59 +0300

    Added styles to wwwroot. Updated layout of MainLayout.

--- src/ScienceAtrium/ScienceAtrium.Presentation/wwwroot/scripts/main-js.js
+++ src/ScienceAtrium/ScienceAtrium.Presentation/wwwroot/scripts/main-js.js
@@ -1,24 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('.main');
-  const header = document.querySelector('.header');
+if (document.documentElement.clientWidth > 992) {
 
-const headerFixed = () => {
-  let scrollTop = window.scrollY;
-  let mainCenter = main.offsetHeight / 2;
-
-  if (scrollTop >= mainCenter) {
-    header.classList.add('fixed')
-    main.style.marginTop = `${header.offsetHeight}px`;
-  } else {
-    header.classList.remove('fixed')
-    main.style.marginTop = `0px`;
-  }
-};
-
-headerFixed();
-  window.addEventListener('scroll', () => {
-    headerFixed();
+  document.addEventListener('DOMContentLoaded', () => {
+    const main = document.querySelector('.main');
+    const header = document.querySelector('.header');
+  
+  const headerFixed = () => {
+    let scrollTop = window.scrollY;
+    let mainCenter = main.offsetHeight / 2;
+  
+    if (scrollTop >= mainCenter) {
+      header.classList.add('fixed')
+      main.style.marginTop = `${header.offsetHeight}px`;
+    } else {
+      header.classList.remove('fixed')
+      main.style.marginTop = `0px`;
+    }
+  };
+  
+  headerFixed();
+    window.addEventListener('scroll', () => {
+      headerFixed();
+    });
   });
-});
 
-
+}
+  
+  
+  
