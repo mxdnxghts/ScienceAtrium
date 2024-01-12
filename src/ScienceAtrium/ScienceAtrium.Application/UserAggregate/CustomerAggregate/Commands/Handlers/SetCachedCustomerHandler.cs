a975b7b21b14b80969905ca2f2fc17624f484ef7
mxdnxght <cloudyfg@gmail.com>
2024-01-12 06:19:34 +0300

    Added handler for SetCachedCustomerCommand. Removed extra scripts.

--- src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/CustomerAggregate/Commands/Handlers/SetCachedCustomerHandler.cs
+++ src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/CustomerAggregate/Commands/Handlers/SetCachedCustomerHandler.cs
@@ -6,13 +6,13 @@ using ScienceAtrium.Domain.UserAggregate.CustomerAggregate;
 
 namespace ScienceAtrium.Application.UserAggregate.CustomerAggregate.Commands.Handlers;
 public class SetCachedCustomerHandler(IDistributedCache _cache, IReader<Customer> _customerReader)
-    : IRequestHandler<SetCachedCustomerCommand>
+	: IRequestHandler<SetCachedCustomerCommand>
 {
-    public async Task Handle(SetCachedCustomerCommand request, CancellationToken cancellationToken)
-    {
-        if (!request.Customer.IsValid(_customerReader))
-            return;
+	public async Task Handle(SetCachedCustomerCommand request, CancellationToken cancellationToken)
+	{
+		if (!request.Customer.IsValid(_customerReader))
+			return;
 
-        await _cache.SetRecordAsync("CachedCustomer", request.Customer, TimeSpan.FromMinutes(10), cancellationToken: cancellationToken);
-    }
+		await _cache.SetRecordAsync("CachedCustomer", request.Customer, TimeSpan.FromMinutes(10), cancellationToken: cancellationToken);
+	}
 }
--- src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/CustomerAggregate/Queries/Handlers/GetCachedCustomerHandler.cs
+++ src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/CustomerAggregate/Queries/Handlers/GetCachedCustomerHandler.cs
@@ -1,11 +1,10 @@
-﻿using AutoMapper;
-using MediatR;
+﻿using MediatR;
 using Microsoft.Extensions.Caching.Distributed;
 using ScienceAtrium.Application.Extensions;
 using ScienceAtrium.Domain.UserAggregate.CustomerAggregate;
 
 namespace ScienceAtrium.Application.UserAggregate.CustomerAggregate.Queries.Handlers;
-public class GetCachedCustomerHandler(IDistributedCache _cache, IMapper _mapper) : IRequestHandler<GetCachedCustomerQuery, Customer>
+public class GetCachedCustomerHandler(IDistributedCache _cache) : IRequestHandler<GetCachedCustomerQuery, Customer>
 {
     public async Task<Customer> Handle(GetCachedCustomerQuery request, CancellationToken cancellationToken)
     {
