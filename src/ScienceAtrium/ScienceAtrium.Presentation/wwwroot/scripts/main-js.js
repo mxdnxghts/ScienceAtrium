316c8c080a197a337fe8cbdda1f00c5a72bb3347
mxdnxght <cloudyfg@gmail.com>
2024-01-14 02:21:13 +0300

    Updated styles and main layout.

--- src/ScienceAtrium/ScienceAtrium.Presentation/wwwroot/scripts/main-js.js
+++ src/ScienceAtrium/ScienceAtrium.Presentation/wwwroot/scripts/main-js.js
@@ -1,21 +1,26 @@
-let login = document.getElementById("login");
-let register = document.getElementById("register");
 let loginBtn = document.getElementById("loginBtn");
 let registerBtn = document.getElementById("registerBtn");
 
 setEventListenersToBtns();
+console.log("Set event listeners");
 setScroller();
+console.log("Set scroller");
 setActiveLinks();
+console.log("Set active linkgs");
 
 function setActiveLinks(){
-    var navMenu = document.getElementById("navMenu");
+    let navMenu = document.getElementById("navMenu");
     let links = navMenu.getElementsByClassName("link");
 
-    for (var i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", function() {
-          var current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          links[i].className += " active";
+    for (const element of links) {
+        element.addEventListener("click", function() {
+            let current = document.getElementsByClassName("active");
+            element.className += " active";
+            console.log(element.className)
+            if (current === undefined) {
+                return;
+            }
+            current[0].className = current[0].className.replace(" active", "");
         });
       }
 }
@@ -36,22 +41,22 @@ function setEventListenersToBtns() {
 
 
 function changeClickedLoginBtnStyle() {
-    login.style.left = "4px";
-    login.style.opacity = 1;
+    //login.style.left = "4px";
+    //login.style.opacity = 1;
 
-    register.style.right = "-520px";
-    register.style.opacity = 0;
+    //register.style.right = "-520px";
+    //register.style.opacity = 0;
 
     loginBtn.className += " white-btn";
     registerBtn.className = "btn";
 }
 
 function changeClickedRegisterBtnStyle() {
-    login.style.left = "-510px";
-    login.style.opacity = 0;
+    //login.style.left = "-510px";
+    //login.style.opacity = 0;
 
-    register.style.right = "5px";
-    register.style.opacity = 1;
+    //register.style.right = "5px";
+    //register.style.opacity = 1;
 
     loginBtn.className = "btn";
     registerBtn.className += " white-btn";
@@ -69,7 +74,7 @@ function setScroller() {
     if (document.documentElement.clientWidth < 539) {
         return;
     }
-    
+
     document.addEventListener('DOMContentLoaded', () => {
         const main = document.querySelector('.main');
         const header = document.querySelector('.header');
