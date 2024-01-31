17421ff410e348646d26db73a758c5bbe6e41689
mxdnxght <cloudyfg@gmail.com>
2024-02-01 00:31:31 +0300

    Added sync caching functions. Updated default TTL to 180 seconds.

--- src/ScienceAtrium/ScienceAtrium.Infrastructure/Extensions/DistributedCacheExtension.cs
+++ src/ScienceAtrium/ScienceAtrium.Infrastructure/Extensions/DistributedCacheExtension.cs
@@ -5,30 +5,52 @@ namespace ScienceAtrium.Infrastructure.Extensions;
 public static class DistributedCacheExtensions
 {
     /// <summary>
-    /// In minutes
+    /// In seconds
     /// </summary>
-    private const int DefaultTimeExpiration = 20;
+    private const int DefaultTimeExpiration = 180;
+    
+    public static void SetRecord<TData>(
+        this IDistributedCache cache,
+        string key,
+        TData data,
+        JsonSerializerSettings? jsonSerializerSettings = null,
+        TimeSpan? absoluteTimeExpirationRelativeToNow = null,
+        TimeSpan? slidingExpiration = null)
+    {
+        var options = GetEntryOptions(absoluteTimeExpirationRelativeToNow, slidingExpiration);
+
+        var jsonData = GetSerializedData(data, jsonSerializerSettings);
+
+        cache.SetString(key, jsonData, options);
+    }
 
     public static async Task SetRecordAsync<TData>(
         this IDistributedCache cache,
         string key,
         TData data,
+        JsonSerializerSettings? jsonSerializerSettings = null,
         TimeSpan? absoluteTimeExpirationRelativeToNow = null,
         TimeSpan? slidingExpiration = null,
         CancellationToken cancellationToken = default)
     {
-        var options = new DistributedCacheEntryOptions
-        {
-            AbsoluteExpirationRelativeToNow = absoluteTimeExpirationRelativeToNow ??
-                TimeSpan.FromMinutes(DefaultTimeExpiration),
-            SlidingExpiration = slidingExpiration,
-        };
+        var options = GetEntryOptions(absoluteTimeExpirationRelativeToNow, slidingExpiration);
 
-        var jsonData = JsonConvert.SerializeObject(data, Formatting.None, new JsonSerializerSettings
-        {
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-        });
+        var jsonData = GetSerializedData(data, jsonSerializerSettings);
+
         await cache.SetStringAsync(key, jsonData, options, cancellationToken);
+    }
+
+    public static TData GetRecord<TData>(
+        this IDistributedCache cache,
+        string key,
+        JsonSerializerSettings? jsonSerializerSettings = null)
+    {
+        var data = cache.GetString(key);
+
+        if (data is null)
+            return default;
+
+        return JsonConvert.DeserializeObject<TData>(data, jsonSerializerSettings);
     }
 
     public static async Task<TData> GetRecordAsync<TData>(
@@ -42,6 +64,27 @@ public static class DistributedCacheExtensions
         if (data is null)
             return default;
 
-		return JsonConvert.DeserializeObject<TData>(data, jsonSerializerSettings);
+        return JsonConvert.DeserializeObject<TData>(data, jsonSerializerSettings);
+    }
+
+    private static DistributedCacheEntryOptions GetEntryOptions(
+        TimeSpan? absoluteTimeExpirationRelativeToNow = null,
+        TimeSpan? slidingExpiration = null)
+    {
+        return new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteTimeExpirationRelativeToNow ??
+                TimeSpan.FromSeconds(DefaultTimeExpiration),
+            SlidingExpiration = slidingExpiration,
+        };
+    }
+
+    private static string GetSerializedData<TData>(TData data, JsonSerializerSettings? jsonSerializerSettings = null)
+    {
+        return JsonConvert.SerializeObject(data, Formatting.None, jsonSerializerSettings ??
+            new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            });
     }
 }
