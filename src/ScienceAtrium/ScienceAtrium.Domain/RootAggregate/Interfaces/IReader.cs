b47fcb7c9298040a9cea3c37e0a8667299b2a95e
mxdnxght <cloudyfg@gmail.com>
2024-02-03 04:06:36 +0300

    Added description to methods Get and GetAsync in IReader/Async.

--- src/ScienceAtrium/ScienceAtrium.Domain/RootAggregate/Interfaces/IReader.cs
+++ src/ScienceAtrium/ScienceAtrium.Domain/RootAggregate/Interfaces/IReader.cs
@@ -1,17 +1,18 @@
 ﻿using System.Linq.Expressions;
-using ScienceAtrium.Domain.RootAggregate;
 
 namespace ScienceAtrium.Domain.RootAggregate.Interfaces;
 
 public interface IReader<TEntity> where TEntity : Entity
 {
-    /// <summary>
-    /// returns entity with specified predicate from database
-    /// </summary>
-    /// <param name="id">entity id</param>
-    /// <param name="predicate">predicate for function expression</param>
-    /// <returns></returns>
-    TEntity Get(Guid? id = null, Expression<Func<TEntity, bool>>? predicate = null);
+	/// <summary>
+	/// returns entity with specified predicate from database
+	/// if you'll pass only a predicate, it will return an entity from the database
+    /// and 'll skip check in cache
+	/// </summary>
+	/// <param name="id">entity id</param>
+	/// <param name="predicate">predicate for function expression</param>
+	/// <returns></returns>
+	TEntity Get(Guid? id = null, Expression<Func<TEntity, bool>>? predicate = null);
 
     /// <summary>
     /// returns true or false depending exists entity in the database or not
--- src/ScienceAtrium/ScienceAtrium.Domain/RootAggregate/Interfaces/IReaderAsync.cs
+++ src/ScienceAtrium/ScienceAtrium.Domain/RootAggregate/Interfaces/IReaderAsync.cs
@@ -1,17 +1,18 @@
 ﻿using System.Linq.Expressions;
-using ScienceAtrium.Domain.RootAggregate;
 
 namespace ScienceAtrium.Domain.RootAggregate.Interfaces;
 
 public interface IReaderAsync<TEntity> where TEntity : Entity
 {
-    /// <summary>
-    /// returns entity with specified predicate from database
-    /// </summary>
-    /// <param name="id">entity id</param>
-    /// <param name="predicate">predicate for function expression</param>
-    /// <returns></returns>
-    Task<TEntity> GetAsync(Guid? id = null, Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default);
+	/// <summary>
+	/// returns entity with specified predicate from database
+	/// if you'll pass only a predicate, it will return an entity from the database
+	/// and 'll skip check in cache
+	/// </summary>
+	/// <param name="id">entity id</param>
+	/// <param name="predicate">predicate for function expression</param>
+	/// <returns></returns>
+	Task<TEntity> GetAsync(Guid? id = null, Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// returns true or false depending exists entity in the database or not
