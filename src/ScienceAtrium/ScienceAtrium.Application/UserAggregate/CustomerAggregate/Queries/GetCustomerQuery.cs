83523899da0c2c46cf9cb2354a074aa3e81370e1
mxdnxght <cloudyfg@gmail.com>
2024-02-07 20:06:33 +0300

    Added general customer's query to take customer from database.

---
+++ src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/CustomerAggregate/Queries/GetCustomerQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using ScienceAtrium.Domain.UserAggregate.CustomerAggregate;
+using System.Linq.Expressions;
+
+namespace ScienceAtrium.Application.UserAggregate.CustomerAggregate.Queries;
+public record GetCustomerQuery(Guid? CustomerId = null, Expression<Func<Customer, bool>>? Predicate = null) : IRequest<Customer>;
