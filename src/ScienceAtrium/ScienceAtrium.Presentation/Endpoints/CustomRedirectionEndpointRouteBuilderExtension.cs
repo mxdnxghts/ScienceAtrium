5f9ddaf4207cac31dd3d314219cd83c4926078ec
mxdnxght <cloudyfg@gmail.com>
2024-03-07 18:54:22 +0300

    Changed launchUrl to "../home-redirect". Added configuration of general endpoints that adds CustomerId to request URI. Added HttpClient "AccountLoginClient" that operates with auth services.

---
+++ src/ScienceAtrium/ScienceAtrium.Presentation/Endpoints/CustomRedirectionEndpointRouteBuilderExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Primitives;
+
+namespace ScienceAtrium.Presentation.Endpoints;
+
+internal static class CustomRedirectionEndpointRouteBuilderExtension
+{
+	public static IEndpointConventionBuilder MapRewroteEndpoints(this IEndpointRouteBuilder endpoints)
+	{
+		var home = endpoints.MapGroup("/");
+		
+		home.MapGet("/home-redirect", async (context) => DefaultRedirect(context, "/home"));
+		home.MapGet("/account-redirect", async (context) => DefaultRedirect(context, "/account"));
+		home.MapGet("/basket-redirect", async (context) => DefaultRedirect(context, "/basket"));
+
+		return home;
+	}
+
+	private static void DefaultRedirect(HttpContext context, PathString path)
+    {
+        IEnumerable<KeyValuePair<string, StringValues>> query = [
+					new ("CustomerId", context.Request.Cookies["CustomerId"] ?? Guid.Empty.ToString())
+				];
+		var redirectUrl = UriHelper.BuildRelative(GetOriginalPath(context.Request.PathBase), path, QueryString.Create(query));
+		context.Response.Redirect(redirectUrl);
+		
+	}
+
+
+    private static string GetOriginalPath(PathString redirectPathString)
+	{
+		return new string(redirectPathString.ToString().ToCharArray().TakeWhile(x => x != '-').ToArray());
+	}
+}
