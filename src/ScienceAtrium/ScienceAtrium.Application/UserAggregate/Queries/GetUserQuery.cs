7ac72f950e0aa375815971b23e410b162472442d
mxdnxght <cloudyfg@gmail.com>
2024-04-22 23:31:11 +0300

    Added new request that returns generalized user. Added method GetExecutorAsync to UserHelper.

---
+++ src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/Queries/GetUserQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using ScienceAtrium.Domain.RootAggregate.Options;
+using ScienceAtrium.Domain.UserAggregate;
+
+namespace ScienceAtrium.Application.UserAggregate.Queries;
+public record GetUserQuery<TUser>(EntityFindOptions<TUser> EntityFindOptions) : IRequest<TUser> where TUser : User;
---
+++ src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/Queries/Handlers/GetUserHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using ScienceAtrium.Domain.RootAggregate.Interfaces;
+using ScienceAtrium.Domain.UserAggregate;
+
+namespace ScienceAtrium.Application.UserAggregate.Queries.Handlers;
+public class GetUserHandler<TUser>(IMapper mapper,
+                                   IReaderAsync<TUser> _reader)
+    : IRequestHandler<GetUserQuery<TUser>, TUser> where TUser : User
+{
+    public async Task<TUser> Handle(GetUserQuery<TUser> request, CancellationToken cancellationToken)
+    {
+        if (request is null || request.EntityFindOptions is null)
+            return mapper.Map<TUser>(User.Default);
+
+        var user = await _reader.GetAsync(request.EntityFindOptions, cancellationToken);
+        return user;
+    }
+}
