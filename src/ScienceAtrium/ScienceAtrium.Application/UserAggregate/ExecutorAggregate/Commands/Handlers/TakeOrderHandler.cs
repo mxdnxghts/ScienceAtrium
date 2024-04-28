ff86c5e075aa4a0f14ce7a3644ee7804ac69926d
mxdnxght <cloudyfg@gmail.com>
2024-04-28 14:32:36 +0300

    Added template for TakeOrderHandler. Added logic that implements finding executor and assigning selected order to executor.

---
+++ src/ScienceAtrium/ScienceAtrium.Application/UserAggregate/ExecutorAggregate/Commands/Handlers/TakeOrderHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using ScienceAtrium.Domain.OrderAggregate;
+using ScienceAtrium.Domain.UserAggregate;
+using ScienceAtrium.Domain.UserAggregate.ExecutorAggregate;
+
+namespace ScienceAtrium.Application.UserAggregate.ExecutorAggregate.Commands.Handlers;
+public class TakeOrderHandler(IOrderRepository<Order> _orderRepository,
+                              IUserRepository<Executor> _executorRepository) : IRequestHandler<TakeOrderCommand>
+{
+    public async Task Handle(TakeOrderCommand request, CancellationToken cancellationToken)
+    {
+        if (request is null || request.Executor is null || request.Order is null)
+            return;
+        if (!request.Executor.IsValid(_executorRepository) || !request.Order.IsValid(_orderRepository))
+            return;
+
+
+    }
+}
